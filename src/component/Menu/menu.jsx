ac97ac435f4db3c28ad5fea7feb747a93d79d312
Chetan Navnath Padhen <padhenchetan@gmail.com>
2023-11-16 16:47:38 +0530

    c

---
+++ src/component/Menu/menu.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+const Menu=()=>{
+    return<>
+    <div className="menu" style={{height:"100vh",width:"17vw",backgroundColor:'whitesmoke'}}>
+      
+            </div>
+    </>
+}
+export default Menu;
---
+++ src/component/navbar/navabr.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+const Navbar=()=>{
+    return<>
+    <div className="navbar" style={{height:"8vh",backgroundColor:'white'}}>
+        <div className="menu" style={{height:"8vh",width:"16.5vw",backgroundColor:'white',display:"flex",justifyContent:"center",alignItems:"center"}}>Menu</div>
+      <div  style={{width:"2px",height:"8vh",backgroundColor:"whitesmoke",marginLeft:"16.7vw"}}> </div>
+            </div>
+    </>
+}
+export default Navbar;
