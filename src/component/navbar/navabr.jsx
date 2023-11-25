2e2714d68f67008fdd5086e2f630bae9a2c296c0
Chetan Navnath Padhen <padhenchetan@gmail.com>
2023-11-25 22:57:53 +0530

    New Menu effect

--- src/component/navbar/navabr.jsx
+++ src/component/navbar/navabr.jsx
@@ -2,16 +2,33 @@ import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container,Row,Col } from "react-bootstrap";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars ,faXmark,faPen,faCircleUser} from '@fortawesome/free-solid-svg-icons';
-import {faBookmark,faMessage} from '@fortawesome/free-regular-svg-icons';
+import { faBars ,faXmark,faPen,faCircleUser,faUser} from '@fortawesome/free-solid-svg-icons';
+import {faBookmark,faMessage,faCalendarDays} from '@fortawesome/free-regular-svg-icons';
+import { faUsers,faPhotoFilm ,faMusic} from '@fortawesome/free-solid-svg-icons';
+import { faYoutube } from '@fortawesome/free-brands-svg-icons';
+const FirstDiv=document.getElementById('FirstDiv');
+const seconddiv=document.getElementById('seconddiv');
+function dispnewblock( k){
+
+   if(k==1){
+ FirstDiv.style.display="block";
+  seconddiv.style.display="none";
+  }
+  else{
+    FirstDiv.style.display="none";
+    seconddiv.style.display="block";
+  
+  }
+}
 
 const Navbar=()=>{
     return<>
-    <div className="navbar" style={{height:"11vh",width:"16vw", borderBottom:'2px solid whitesmoke',borderRight:'2px solid whitesmoke'}}>
+    <div id="FirstDiv" className="div" style={{display:"none"}}>
+    <div className="navbar" style={{height:"11vh", width:"16vw", borderBottom:'2px solid whitesmoke',borderRight:'2px solid whitesmoke'}}>
     <Container  className=" d-flex align-item-center">
-   <Col className="col-2 d-flex  justify-content-end">   <FontAwesomeIcon icon={faBars} /> </Col>
+   <Col className="col-2 d-flex  justify-content-end">   <FontAwesomeIcon style={{cursor:'pointer'}} icon={faBars} /> </Col>
    <Col className="col-4 d-flex  justify-content-center">Menu</Col>
-   <Col className="col-4 d-flex  justify-content-end"><FontAwesomeIcon style={{color:"#cccccc"}} icon={ faXmark}/></Col>
+   <Col onClick={()=>{dispnewblock(0)}} className="col-4 d-flex  justify-content-end"><FontAwesomeIcon style={{color:"#cccccc"}} icon={ faXmark}/></Col>
     </Container>
     </div>
     <div className="navbar" style={{height:"50vh",width:"16vw", borderBottom:'2px solid whitesmoke',borderRight:'2px solid whitesmoke'}}>
@@ -21,50 +38,110 @@ const Navbar=()=>{
    <Col className="col-2 d-flex  justify-content-end"><FontAwesomeIcon style={{color:"#cccccc"}} icon={ faPen}/></Col>
     </Container>
     <Container  className="">
-   <Col className="col-2 d-flex  justify-content-end">   <FontAwesomeIcon style={{color:"#cccccc"}} icon={faBookmark} /> </Col>
-   <Col className="col-7 d-flex  justify-content-center">News Feed</Col>
-   <Col className="col-2 d-flex  justify-content-end"><FontAwesomeIcon style={{color:"#cccccc"}} icon={ faPen}/></Col>
+   <Col className="col-2 d-flex  justify-content-end">   <FontAwesomeIcon style={{}} icon={faBookmark} /> </Col>
+   <Col className="col-6 d-flex  ">News Feed</Col>
+   <Col className="col-2 d-flex  justify-content-end"><FontAwesomeIcon style={{color:"#cccccc"}} /></Col>
     </Container>
     <Container  className="">
    <Col className="col-2 d-flex  justify-content-end">   <FontAwesomeIcon icon={faMessage} /> </Col>
-   <Col className="col-7 d-flex  justify-content-center">Message</Col>
-   <Col className="col-2 d-flex  justify-content-end"><FontAwesomeIcon style={{color:"#cccccc"}} icon={ faPen}/></Col>
+   <Col className="col-6 d-flex  ">Message</Col>
+   <Col className="col-2 d-flex  justify-content-end"><FontAwesomeIcon style={{color:"#cccccc"}} /></Col>
     </Container>
     <Container  className="">
-   <Col className="col-2 d-flex  justify-content-end">   <FontAwesomeIcon icon={faCircleUser} /> </Col>
-   <Col className="col-7 d-flex  justify-content-center">Message</Col>
-   <Col className="col-2 d-flex  justify-content-end"><FontAwesomeIcon style={{color:"#cccccc"}} icon={ faPen}/></Col>
+   <Col className="col-2 d-flex  justify-content-end">   <FontAwesomeIcon icon={faCalendarDays} /> </Col>
+   <Col className="col-6 d-flex  ">Events</Col>
+   <Col className="col-2 d-flex  justify-content-end"><FontAwesomeIcon style={{color:"#cccccc"}} /></Col>
     </Container>
+    <Container  className="">
+   <Col className="col-2 d-flex  justify-content-end">   <FontAwesomeIcon icon={faUser} /> </Col>
+   <Col className="col-6 d-flex  ">Friend</Col>
+   <Col className="col-2 d-flex  justify-content-end"><FontAwesomeIcon style={{color:"#cccccc"}} /></Col>
+    </Container>
+    <Container  className=""><FontAwesomeIcon icon="fa-regular " />
+   <Col className="col-2 d-flex  justify-content-end">   <FontAwesomeIcon icon={faUsers} /> </Col>
+   <Col className="col-6 d-flex  ">Groups</Col>
+   <Col className="col-2 d-flex  justify-content-end"><FontAwesomeIcon style={{color:"#cccccc"}} /></Col>
+    </Container>
+    <Container  className="">
+   <Col className="col-2 d-flex  justify-content-end">   <FontAwesomeIcon icon={faPhotoFilm} /> </Col>
+   <Col className="col-6 d-flex  ">Photo</Col>
+   <Col className="col-2 d-flex  justify-content-end"><FontAwesomeIcon style={{color:"#cccccc"}} /></Col>
+    </Container>
+    <Container  className="">
+   <Col className="col-2 d-flex  justify-content-end">   <FontAwesomeIcon icon={faMusic} /> </Col>
+   <Col className="col-6 d-flex  ">Music</Col>
+   <Col className="col-2 d-flex  justify-content-end"><FontAwesomeIcon style={{color:"#cccccc"}} /></Col>
+    </Container>
+    <Container  className="">
+   <Col className="col-2 d-flex  justify-content-end">   <FontAwesomeIcon icon={faYoutube} /> </Col>
+   <Col className="col-6 d-flex  ">Video</Col>
+   <Col className="col-2 d-flex  justify-content-end"><FontAwesomeIcon style={{color:"#cccccc"}} /></Col>
+    </Container>
+    <Container  className="">
+   <Col className="col-2 d-flex  justify-content-end">   <FontAwesomeIcon icon={faCalendarDays} /> </Col>
+   <Col className="col-6 d-flex  ">Calender</Col>
+   <Col className="col-2 d-flex  justify-content-end mb-5"><FontAwesomeIcon style={{color:"#cccccc"}} /></Col>
+    </Container>
+
+    </div>
+    <div className="navbar" style={{height:"50vh",width:"16vw", borderBottom:'2px solid whitesmoke',borderRight:'2px solid whitesmoke'}}>
     <Container  className="">
    <Col className="col-2 d-flex  justify-content-end">   <FontAwesomeIcon icon={faCircleUser} /> </Col>
    <Col className="col-7 d-flex  justify-content-center">Chetan Padhen</Col>
    <Col className="col-2 d-flex  justify-content-end"><FontAwesomeIcon style={{color:"#cccccc"}} icon={ faPen}/></Col>
     </Container>
     <Container  className="">
-   <Col className="col-2 d-flex  justify-content-end">   <FontAwesomeIcon icon={faCircleUser} /> </Col>
-   <Col className="col-7 d-flex  justify-content-center">Chetan Padhen</Col>
-   <Col className="col-2 d-flex  justify-content-end"><FontAwesomeIcon style={{color:"#cccccc"}} icon={ faPen}/></Col>
+   <Col className="col-2 d-flex  justify-content-end">   <FontAwesomeIcon style={{}} icon={faBookmark} /> </Col>
+   <Col className="col-6 d-flex  ">News Feed</Col>
+   <Col className="col-2 d-flex  justify-content-end"><FontAwesomeIcon style={{color:"#cccccc"}} /></Col>
     </Container>
     <Container  className="">
-   <Col className="col-2 d-flex  justify-content-end">   <FontAwesomeIcon icon={faCircleUser} /> </Col>
-   <Col className="col-7 d-flex  justify-content-center">Chetan Padhen</Col>
-   <Col className="col-2 d-flex  justify-content-end"><FontAwesomeIcon style={{color:"#cccccc"}} icon={ faPen}/></Col>
+   <Col className="col-2 d-flex  justify-content-end">   <FontAwesomeIcon icon={faMessage} /> </Col>
+   <Col className="col-6 d-flex  ">Message</Col>
+   <Col className="col-2 d-flex  justify-content-end"><FontAwesomeIcon style={{color:"#cccccc"}} /></Col>
     </Container>
     <Container  className="">
-   <Col className="col-2 d-flex  justify-content-end">   <FontAwesomeIcon icon={faCircleUser} /> </Col>
-   <Col className="col-7 d-flex  justify-content-center">Chetan Padhen</Col>
-   <Col className="col-2 d-flex  justify-content-end"><FontAwesomeIcon style={{color:"#cccccc"}} icon={ faPen}/></Col>
+   <Col className="col-2 d-flex  justify-content-end">   <FontAwesomeIcon icon={faCalendarDays} /> </Col>
+   <Col className="col-6 d-flex  ">Events</Col>
+   <Col className="col-2 d-flex  justify-content-end"><FontAwesomeIcon style={{color:"#cccccc"}} /></Col>
     </Container>
     <Container  className="">
-   <Col className="col-2 d-flex  justify-content-end">   <FontAwesomeIcon icon={faCircleUser} /> </Col>
-   <Col className="col-7 d-flex  justify-content-center">Chetan Padhen</Col>
-   <Col className="col-2 d-flex  justify-content-end"><FontAwesomeIcon style={{color:"#cccccc"}} icon={ faPen}/></Col>
+   <Col className="col-2 d-flex  justify-content-end">   <FontAwesomeIcon icon={faUser} /> </Col>
+   <Col className="col-6 d-flex  ">Friend</Col>
+   <Col className="col-2 d-flex  justify-content-end"><FontAwesomeIcon style={{color:"#cccccc"}} /></Col>
+    </Container>
+    <Container  className=""><FontAwesomeIcon icon="fa-regular " />
+   <Col className="col-2 d-flex  justify-content-end">   <FontAwesomeIcon icon={faUsers} /> </Col>
+   <Col className="col-6 d-flex  ">Groups</Col>
+   <Col className="col-2 d-flex  justify-content-end"><FontAwesomeIcon style={{color:"#cccccc"}} /></Col>
     </Container>
     <Container  className="">
-   <Col className="col-2 d-flex  justify-content-end">   <FontAwesomeIcon icon={faCircleUser} /> </Col>
-   <Col className="col-7 d-flex  justify-content-center">Chetan Padhen</Col>
-   <Col className="col-2 d-flex  justify-content-end"><FontAwesomeIcon style={{color:"#cccccc"}} icon={ faPen}/></Col>
+   <Col className="col-2 d-flex  justify-content-end">   <FontAwesomeIcon icon={faPhotoFilm} /> </Col>
+   <Col className="col-6 d-flex  ">Photo</Col>
+   <Col className="col-2 d-flex  justify-content-end"><FontAwesomeIcon style={{color:"#cccccc"}} /></Col>
     </Container>
+    <Container  className="">
+   <Col className="col-2 d-flex  justify-content-end">   <FontAwesomeIcon icon={faMusic} /> </Col>
+   <Col className="col-6 d-flex  ">Music</Col>
+   <Col className="col-2 d-flex  justify-content-end"><FontAwesomeIcon style={{color:"#cccccc"}} /></Col>
+    </Container>
+    <Container  className="">
+   <Col className="col-2 d-flex  justify-content-end">   <FontAwesomeIcon icon={faYoutube} /> </Col>
+   <Col className="col-6 d-flex  ">Video</Col>
+   <Col className="col-2 d-flex  justify-content-end"><FontAwesomeIcon style={{color:"#cccccc"}} /></Col>
+    </Container>
+    <Container  className="">
+   <Col className="col-2 d-flex  justify-content-end">   <FontAwesomeIcon icon={faCalendarDays} /> </Col>
+   <Col className="col-6 d-flex  ">Calender</Col>
+   <Col className="col-2 d-flex  justify-content-end mb-5"><FontAwesomeIcon style={{color:"#cccccc"}} /></Col>
+    </Container>
+
+    </div>
+    </div>
+    <div id="seconddiv" className="container-fluid  "  >
+    <div className="col-1 " onClick={()=>{dispnewblock(1)}}style={{backgroundColor:"yellow",height:"100vh", width:"55px"} }>
+    <FontAwesomeIcon  style={{cursor:'pointer',display:'flex',justifyContent:"center"}} icon={faBars} /> 
+    </div>
     </div>
     </>
 }
